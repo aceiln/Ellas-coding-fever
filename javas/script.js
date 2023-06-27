75ab7ed4ec176f3ce7c300e7e93b0f841654629f
Ella <ellathitotran@gmail.com>
2023-06-26 19:26:59 -0500

    added paragraph break box, works sectionwith grid, and contact

--- javas/script.js
+++ javas/script.js
@@ -1,9 +1,8 @@
-/* Set the width of the sidebar to 250px (show it) */
+
 function openNav() {
     document.getElementById("mySidepanel").style.width = "180px";
   }
   
-  /* Set the width of the sidebar to 0 (hide it) */
   function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
   }
